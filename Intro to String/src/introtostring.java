00c8b25e2d6409360ee5a98ea4a07b712c1fe51f
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2018-08-30 08:50:02 -0600

    first

---
+++ Intro to String/src/introtostring.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class introtostring
+	{
+
+		public static void main(String[] args)
+			{
+				challenge1();
+
+			}
+
+		private static void challenge1()
+			{
+				System.out.println("Write somethings in uppercase");
+				Scanner userInput1 = new Scanner(System.in);
+				String uppercase = userInput1.nextLine();
+				
+				toLowerCase();
+				
+			}
+
+	}
