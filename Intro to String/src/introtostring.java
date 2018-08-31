157b64ab30211c94bddbaa2ae4591abcfefb8344
jiazhi.sun <jiazhi.sun@mu-c1-16.mullen.pvt.k12.co.us>
2018-08-31 08:40:43 -0600

    challenge3

--- Intro to String/src/introtostring.java
+++ Intro to String/src/introtostring.java
@@ -6,17 +6,66 @@ public class introtostring
 		public static void main(String[] args)
 			{
 				challenge1();
+				challenge2();
+				challenge3();
+			}
 
+		private static void challenge3()
+			{
+				System.out.println("vowels checker, write a sentence");
+				Scanner userInput1 = new Scanner(System.in);
+				String sentence = userInput1.nextLine();
+				String vowels1 = "a";
+				String vowels2 = "e";
+				String vowels3 = "i";
+				String vowels4 = "o";
+				String vowels5 = "u";
+				
+				for()
+				
+			}
+
+		private static void challenge2()
+			{
+				
+				System.out.println("Write somethings in uppercase");
+				Scanner userInput1 = new Scanner(System.in);
+				String uppercase = userInput1.nextLine();
+				
+				
+				
+				String lowercase = uppercase.toLowerCase();
+				int long1 = uppercase.length();
+				System.out.println(uppercase.substring(0, 1));
+				 
+				for(int i = 1; i < long1; i ++ ){
+					
+					System.out.println( lowercase.substring(i ,i + 1));
+				}
 			}
 
 		private static void challenge1()
 			{
 				System.out.println("Write somethings in uppercase");
-				Scanner userInput1 = new Scanner(System.in);
-				String uppercase = userInput1.nextLine();
+				Scanner userInput2 = new Scanner(System.in);
+				String uppercase = userInput2.nextLine();
 				
-				toLowerCase();
+				
+				
+				String lowercase = uppercase.toLowerCase();
+				
+				System.out.println(uppercase.substring(0, 1) + lowercase.substring(1));
 				
 			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
